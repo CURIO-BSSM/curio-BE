2bf9d4ac2179d8f0c838f2e3adfe99fc2e22abc5
Hyun731 <gilhyun.gwon0731@gmail.com>
2025-08-21 09:29:41 +0900

    refactor:주석 추가

--- app/crud/question_crud.py
+++ app/crud/question_crud.py
@@ -3,6 +3,7 @@ from app.models.models import Unit, Question
 
 
 def get_quiz_by_unit(db: Session, unit_id: int):
+    #db.query(model) -> 모델에 대한 쿼리를 날리는 것, filter은 where이런 느낌
     unit = db.query(Unit).filter(Unit.id == unit_id).first()
     if not unit:
         return None
