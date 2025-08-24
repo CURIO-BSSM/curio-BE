2e59c4a3b46349ee2e639ae4f1e75422173a6e79
Hyun731 <gilhyun.gwon0731@gmail.com>
2025-08-25 08:19:20 +0900

    chore:main.py로 실행 가능하게 수정

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.core.config import engine, Base
+import uvicorn
 
 app = FastAPI(title="Science Project API")
 
@@ -12,3 +13,6 @@ Base.metadata.create_all(bind=engine)
 @app.get("/")
 def root():
     return {"message": ""}
+
+if __name__ == "__main__":
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
